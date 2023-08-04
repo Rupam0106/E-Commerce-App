d876dffe7017d9530749a49a6d11e8e26851ef16
Rupam0106 <rupam.gupta@rudrainnovative.in>
2023-08-04 16:07:34 +0530

    middleware updated

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userModel = require("../models/userModel");
 exports.isAuthenticate = catchAsyncError(async (req, res, next) => {
   const { refreshToken } = req.cookies;
   const accessToken = req.headers["authorization"];
-  if (!refreshToken && !accessToken) {
+  if (!refreshToken || !accessToken) {
     return next(new ErrorHandler("Session Expired please login in Again", 401));
   }
   const decodedToken = jwt.verify(refreshToken, process.env.JWT_SECRET);
